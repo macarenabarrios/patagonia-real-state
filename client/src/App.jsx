a0733e5661690686337e21e57dadbd3582957c84
Macarena <maquibarrios@gmail.com>
2024-02-23 20:29:10 -0300

    Creacion componente header (navbar)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,18 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import SignOut from './pages/SignOut';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sobre-nosotros' element={<About />} />
+        <Route path='/perfil' element={<Profile />} />
+        <Route path='/iniciar-sesion' element={<SignIn />} />
+        <Route path='/crear-usuario' element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
