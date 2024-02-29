40e5671b4619bf3124306feeb6729ef14c92bdf4
Macarena <maquibarrios@gmail.com>
2024-02-29 15:08:13 -0300

    Funcionalidad de inicio de sesion creada

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = Router();
 
+router.post('/signin', signin);
 router.post('/signup', signup);
 
 export default router;
--- api/services/auth.services.js
+++ api/services/auth.services.js
@@ -1,5 +1,31 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import customError from '../middlewares/customError.middleware.js';
+import jwt from 'jsonwebtoken';
+
+const generateToken = async (user) => {
+  return jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: "24h" }
+  )
+};
+
+const signin = async (email, password) => {
+  try {
+    const validUser = await User.findOne({
+      email
+    });
+    if (!validUser) return next(customError(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(customError(404, 'Wrong credentials!'));
+    const token = await generateToken(validUser);
+    const { password: pass, ...rest } = validUser._doc;
+    return { user: rest, token };
+  } catch (error) {
+    throw error;
+  }
+};
 
 const signup = async (user) => {
   try {
@@ -14,5 +40,6 @@ const signup = async (user) => {
 };
 
 export const authService = {
+  signin,
   signup
 };
