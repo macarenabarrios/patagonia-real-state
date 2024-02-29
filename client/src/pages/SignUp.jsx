ddc6b91d292d51486737190b51d04c602cb64322
Macarena <maquibarrios@gmail.com>
2024-02-29 00:47:35 -0300

    Pagina de registro finalizada

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>
-
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Registrarse</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='usuario' className='border p-3 rpimded-lg' id='username' />
+        <input type='text' placeholder='email' className='border p-3 rpimded-lg' id='email' />
+        <input type='text' placeholder='contraseña' className='border p-3 rpimded-lg' id='password' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Crear usuario</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Ya tiene una cuenta?</p>
+        <Link to={'/iniciar-sesion'}>
+          <span className='text-blue-700'>Iniciar sesión</span>
+        </Link>
+      </div>
     </div>
   )
 }
